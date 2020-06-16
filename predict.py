59e43aeea01ce7e708e93750aca24e2651b47ed4
Alejo Ortiz <davidalejandro64@gmail.com>
2020-06-16 15:37:57 -0500

    first commit

---
+++ predict.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+import keras
+from keras.initializers import glorot_uniform
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+
+longitud, altura = 100, 100
+modelo = './modelo/modelo.h5'
+pesos_modelo = './modelo/pesos.h5'
+cnn = tf.keras.models.load_model(modelo)
+cnn.load_weights(pesos_modelo)
+
+def predict(file):
+  x = load_img(file, target_size=(longitud, altura))
+  x = img_to_array(x)
+  x = np.expand_dims(x, axis=0)
+  array = cnn.predict(x)
+  result = array[0]
+  answer = np.argmax(result)
+  if answer == 0:
+    print("pred: Cerca")
+  elif answer == 1:
+    print("pred: Lejos")
+  elif answer == 2:
+    print("pred: Medio")
+  elif answer == 3:
+    print("pred: Nada")
+
+  return answer
+
+predict('prueba2.jpg')
